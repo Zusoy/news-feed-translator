e3d824bab789dae31f2b7a1bc830a93355c3fe8e
Grégoire Drapeau <gregoire.drapeau79@gmail.com>
2023-08-11 17:25:35 +0200

    Init API database and doctrine ORM (#3)

---
+++ apps/api/src/Domain/Identity/Identifier.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Identity;
+
+use Assert\Assert;
+use Ramsey\Uuid\Uuid;
+use Stringable;
+
+final class Identifier implements Stringable
+{
+    public const PATTERN = '\A[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}\z';
+
+    public static function generate(): self
+    {
+        return new self(UUid::uuid4()->toString());
+    }
+
+    public function __construct(private readonly string $id)
+    {
+        Assert::that($id, defaultPropertyPath: 'id')
+            ->notEmpty()
+            ->regex(sprintf('/%s/', self::PATTERN));
+    }
+
+    public function __toString(): string
+    {
+        return $this->id;
+    }
+}
